5a55eb34f0db542c01ec6c577c10202659706154
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-01 00:29:38 +0600

    copyright symble added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <p>
-        <small>copyright of my news</small>
+      <p className="">
+        <small>&copy;copyright of my news</small>
       </p>
     </div>
   );
