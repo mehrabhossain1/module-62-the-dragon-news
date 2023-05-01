f7db08f19df7201099341e0e53d88ead323422bc
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-01 22:44:24 +0600

    react icons installed

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
-import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import Marquee from "react-fast-marquee";
 
 const Header = () => {
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="text-center">
         <img src={logo} alt="" />
         <p className="text-secondary">
-          <small>Journalism Without Fear or Favour</small>
+          <small>Journalism Without Fear or Favor</small>
         </p>
         <p>{moment().format("dddd, MMMM D, YYYY")}</p>
       </div>
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { FaGoogle, FaGithub } from "react-icons/fa";
 
 const RightNav = () => {
   return (
     <div>
-      <h2>Right Nav</h2>
+      <h4>Login With</h4>
+      <Button className="mb-2" variant="outline-primary">
+        <FaGoogle /> Login with Google
+      </Button>
+      <Button variant="outline-secondary">
+        <FaGithub /> Login with GitHub
+      </Button>
     </div>
   );
 };
