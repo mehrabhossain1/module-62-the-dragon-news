7a87ccd0e61cfb80371eca841b07f55d5e862d53
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-05 23:47:10 +0600

    63.3

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
+import NewsCard from "../NewsCard/NewsCard";
 
 const Category = () => {
   const { id } = useParams();
+  const categoryNews = useLoaderData();
+  // console.log(categoryNews);
+
   return (
     <div>
-      <h2>This is a category: {id}</h2>
+      <h2>This category News: {categoryNews.length}</h2>
+      {categoryNews.map((news) => (
+        <NewsCard key={news._id} news={news}></NewsCard>
+      ))}
     </div>
   );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -17,6 +17,8 @@ const router = createBrowserRouter([
       {
         path: "category/:id",
         element: <Category></Category>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/categories/${params.id}`),
       },
     ],
   },
@@ -25,7 +27,7 @@ const router = createBrowserRouter([
     element: <NewsLayout></NewsLayout>,
     children: [
       {
-        path: "/:id",
+        path: ":id",
         element: <News></News>,
       },
     ],
